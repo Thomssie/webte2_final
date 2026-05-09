8407ed23d91bbfa1222936136e8b81d151ba090c
Tibor Vnuk <xvnuk@stuba.sk>
2026-05-09 23:07:07 +0200

    Add inverted pendulum simulation

---
+++ resources/js/config/animation.js
@@ -0,0 +1,6 @@
+const animationConfig = {
+    // Rychlost prehravania simulacie. Hodnota 1 znamena realny cas, 0.5 polovicnu rychlost.
+    playbackSpeed: 0.35,
+};
+
+export default animationConfig;
